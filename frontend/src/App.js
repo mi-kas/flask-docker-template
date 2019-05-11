ea8a496048f804499cab7da44b0104fc7876c7f2
mi-kas <mi.kaserer@gmail.com>
2019-05-11 13:36:21 +0200

    React frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+        <p>You can make calls to the flask server using the url http://0.0.0.0:5000</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
